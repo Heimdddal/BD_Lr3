efab442e08935ef54d30b942808729256cea1445
Heimdal <dimasavolin@gmail.com>
2023-11-16 18:46:41 +0300

    finish info out

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,9 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Reflection;
+using static BD_Lr3.Rootobject;
+using System.Security.Policy;
+using System.Xml.XPath;
 
 namespace BD_Lr3
 {
@@ -40,34 +43,101 @@ namespace BD_Lr3
 
             string content = await response.Content.ReadAsStringAsync();
 
-            var bit = JsonConvert.DeserializeObject<BitcoinPrice>(content);
+            var bit = JsonConvert.DeserializeObject<Rootobject>(content);
 
             ResultTBlock.Text = GetFieldsInfo(bit);
             
         }
 
-        private List<Type> GetInnerClasses()
+        public string GetFieldsInfo(Rootobject obj)
         {
-            Type myClassType = typeof(BitcoinPrice);
+            string result = $"Bitcoin Price\n";
 
-            return myClassType
-                .GetNestedTypes(BindingFlags.Public | BindingFlags.NonPublic)
-                .Where(t => t.IsClass)
-                .ToList();
+            result += obj.CombineInfo();
+
+            return result;
         }
+    }
 
-        public string GetFieldsInfo(BitcoinPrice obj)
+    public class Time
+    {
+        public string updated { get; set; }
+        public DateTime updatedISO { get; set; }
+        public string updateduk { get; set; }
+
+        public string GetInfo()
         {
-            var innerClasses = GetInnerClasses();
+            string result = $"{this.GetType().Name}\n";
+            result += $"updated: {this.updated}\nupdated ISO: {this.updatedISO}\nupdated uk: {this.updateduk}\n";
+            return result;
+        }
+    }
 
-            obj.
+    public class Bpi
+    {
+        public USD USD { get; set; }
+        public GBP GBP { get; set; }
+        public EUR EUR { get; set; }
+    }
 
-            string result = $"Class: {obj.GetType().Name}\n";
-                
-            foreach (var innerClass in innerClasses)
-            {
-                result += $"{innerClass.Name}: {GetValue(obj)}\n";
-            }
+    public class USD
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public float rate_float { get; set; }
+
+        public string GetInfo()
+        {
+            string result = $"{this.GetType().Name}\n";
+            result += $"code: {this.code}\nsymbol: {this.symbol}\nrate: {this.rate}\ndescription: {this.description}\n rate float: {this.rate_float}\n";
+            return result;
+        }
+    }
+
+    public class GBP
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public float rate_float { get; set; }
+
+        public string GetInfo()
+        {
+            string result = $"{this.GetType().Name}\n";
+            result += $"code: {this.code}\nsymbol: {this.symbol}\nrate: {this.rate}\ndescription: {this.description}\n rate float: {this.rate_float}\n";
+            return result;
+        }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public float rate_float { get; set; }
+
+        public string GetInfo()
+        {
+            string result = $"{this.GetType().Name}\n";
+            result += $"code: {this.code}\nsymbol: {this.symbol}\nrate: {this.rate}\ndescription: {this.description}\n rate float: {this.rate_float}\n";
+            return result;
+        }
+    }
+
+    public class Rootobject
+    {
+        public Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public Bpi bpi { get; set; }
+
+        public string CombineInfo()
+        {
+            string result = this.time.GetInfo() + disclaimer + '\n' + chartName + '\n' + this.bpi.USD.GetInfo() + this.bpi.EUR.GetInfo() + this.bpi.GBP.GetInfo();
             return result;
         }
     }
